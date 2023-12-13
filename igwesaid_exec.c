ba2176b976f0b281e0201459aa368584c4cd5f93
Youssef Oussaid <oussaid-youssef@outlook.com>
2023-12-13 11:49:17 +0100

    a

--- igwesaid_exec.c
+++ igwesaid_exec.c
@@ -1,7 +1,7 @@
 #include "igwesaid.h"
 
 /*
- * igwesaid_exec - This function executes the command 
+ * igwesaid_exec - This function executes the command
  * @input: this takes on an input from the user
  *
  * Return: this returns void
@@ -15,13 +15,16 @@ void igwesaid_exec(const char *input)
 	{
 		perror("fork");
 		exit(EXIT_FAILURE);
-	}else if (sub_process == 0) 
-		{
-			execve(input, input, NULL);
-			perror("execve");
-			exit(EXIT_FAILURE);
+	}
+	else if (sub_process == 0)
+	{
+		execve(input, input, NULL);
+		perror("execve");
+		exit(EXIT_FAILURE);
 
-		}else {
-			wait (NULL);	
-		}
+	}
+	else
+	{
+		wait (NULL);	
+	}
 }
